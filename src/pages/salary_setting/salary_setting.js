8858222b3c731e78f7f6d73c4a6e1eb7849fcd37
yerim8373 <yerim8373@naver.com>
2024-07-25 20:38:18 +0900

    feat: 급여대장 - 지급항목 json 파일 생성 (db.json) [#21]

--- src/pages/salary_setting/salary_setting.js
+++ src/pages/salary_setting/salary_setting.js
@@ -1,6 +1,6 @@
 // const lastDayInput = document.querySelector('#last-day-select');
 
-// 말일 checkbox 선택 시 input 비활성화
+// 급여대장 - 말일 checkbox 선택 시 input 비활성화
 $(function () {
   $('#last-day-select').on('click', function () {
     if ($(this).is(':checked')) {
@@ -10,5 +10,20 @@ $(function () {
       $('#day-input').prop('disabled', false);
       $('#day-input').prop('value', '10');
     }
+  });
+});
+
+// 지급항목 - 지급항목 페이지 클릭 시 지급항목 리스트 출력
+$(function () {
+  $('#payment-item-tab').on('click', function () {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3000/paymentItems',
+      success: function (response) {
+        $.each(response, function (index, item) {
+          console.log(item.name);
+        });
+      },
+    });
   });
 });
